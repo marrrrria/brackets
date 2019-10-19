7597686dccdd73f4232623545e09310abcb0ebea
Masha <valzyr48@gmail.com>
2019-10-19 16:30:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const configX = bracketsConfig.map(item => item.join(''));
+  let brackets = str;
+  let length;
+  do {
+    length = brackets.length;
+    configX.forEach(item => {
+      brackets = brackets.replace(item, '');
+    });
+  } while (length !== brackets.length);
+  return Boolean(!brackets);
 }
+
